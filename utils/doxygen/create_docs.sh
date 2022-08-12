c8e0bd443ab40db7840418f6ae531323812d800d
James Bristow <james.bristow@plantandfood.co.nz>
2022-08-12 19:25:00 +1200

    Replacing module w/ class in xl and Java files

--- utils/doxygen/create_docs.sh
+++ utils/doxygen/create_docs.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 ###################################################################
+# Functions
+###################################################################
+
+replace_module() {
+  find . -type f -name "$1" -print0 | xargs -0 sed -i "s/module/class/g"
+}
+
+###################################################################
 # Main
 ###################################################################
 
@@ -20,7 +28,10 @@ cp -r ../../src/* docs
 cp Doxyfile docs
 cd docs
 
-find . -type f -name "*.rgg" -print0 | xargs -0 sed -i "s/module/class/g"
+replace_module "*.rgg"
+replace_module "*.xl"
+replace_module "*.java"
+
 doxygen
 
 echo; echo "Open $(pwd)/html/index.html"
